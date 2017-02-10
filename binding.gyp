d1eb05f145f5711f75123c7d35773f4646d27f94
Alexander Blair <alex@snipanet.com>
2017-02-10 14:23:31 -0800

    Initial commit and migration from Wolf9466

---
+++ binding.gyp
@@ -0,0 +1,30 @@
+{
+    "targets": [
+        {
+            "target_name": "multihashing",
+            "sources": [
+                "multihashing.cc",
+                "cryptonight.c",
+                "sha3/sph_keccak.c",
+                "crypto/oaes_lib.c",
+                "crypto/c_keccak.c",
+                "crypto/c_groestl.c",
+                "crypto/c_blake256.c",
+                "crypto/c_jh.c",
+                "crypto/c_skein.c",
+                "crypto/hash.c",
+                "crypto/aesb.c"
+            ],
+            "include_dirs": [
+                "crypto",
+                "<!(node -e \"require('nan')\")",
+            ],
+			"cflags_c": [
+				"-std=gnu11 -march=native -fPIC -m64"
+			],
+            "cflags_cc": [
+                "-std=gnu++11 -fPIC -m64"
+            ],
+        }
+    ]
+}
---
+++ cryptonight.c
@@ -0,0 +1,464 @@
+// Copyright (c) 2012-2013 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "crypto/oaes_lib.h"
+#include "crypto/c_keccak.h"
+#include "crypto/c_groestl.h"
+#include "crypto/c_blake256.h"
+#include "crypto/c_jh.h"
+#include "crypto/c_skein.h"
+#include "crypto/int-util.h"
+#include "crypto/hash-ops.h"
+#include <x86intrin.h>
+
+#define MEMORY         (1 << 21) /* 2 MiB */
+#define ITER           (1 << 20)
+#define AES_BLOCK_SIZE  16
+#define AES_KEY_SIZE    32 /*16*/
+#define INIT_SIZE_BLK   8
+#define INIT_SIZE_BYTE (INIT_SIZE_BLK * AES_BLOCK_SIZE)
+
+#pragma pack(push, 1)
+union cn_slow_hash_state {
+    union hash_state hs;
+    struct {
+        uint8_t k[64];
+        uint8_t init[INIT_SIZE_BYTE];
+    };
+};
+#pragma pack(pop)
+
+static void do_blake_hash(const void* input, size_t len, char* output) {
+    blake256_hash((uint8_t*)output, input, len);
+}
+
+void do_groestl_hash(const void* input, size_t len, char* output) {
+    groestl(input, len * 8, (uint8_t*)output);
+}
+
+static void do_jh_hash(const void* input, size_t len, char* output) {
+    int r = jh_hash(HASH_SIZE * 8, input, 8 * len, (uint8_t*)output);
+    assert(SUCCESS == r);
+}
+
+static void do_skein_hash(const void* input, size_t len, char* output) {
+    int r = c_skein_hash(8 * HASH_SIZE, input, 8 * len, (uint8_t*)output);
+    assert(SKEIN_SUCCESS == r);
+}
+
+static void (* const extra_hashes[4])(const void *, size_t, char *) = {
+    do_blake_hash, do_groestl_hash, do_jh_hash, do_skein_hash
+};
+
+static inline void ExpandAESKey256_sub1(__m128i *tmp1, __m128i *tmp2)
+{
+	__m128i tmp4;
+	*tmp2 = _mm_shuffle_epi32(*tmp2, 0xFF);
+	tmp4 = _mm_slli_si128(*tmp1, 0x04);
+	*tmp1 = _mm_xor_si128(*tmp1, tmp4);
+	tmp4 = _mm_slli_si128(tmp4, 0x04);
+	*tmp1 = _mm_xor_si128(*tmp1, tmp4);
+	tmp4 = _mm_slli_si128(tmp4, 0x04);
+	*tmp1 = _mm_xor_si128(*tmp1, tmp4);
+	*tmp1 = _mm_xor_si128(*tmp1, *tmp2);
+}
+
+static inline void ExpandAESKey256_sub2(__m128i *tmp1, __m128i *tmp3)
+{
+	__m128i tmp2, tmp4;
+	
+	tmp4 = _mm_aeskeygenassist_si128(*tmp1, 0x00);
+	tmp2 = _mm_shuffle_epi32(tmp4, 0xAA);
+	tmp4 = _mm_slli_si128(*tmp3, 0x04);
+	*tmp3 = _mm_xor_si128(*tmp3, tmp4);
+	tmp4 = _mm_slli_si128(tmp4, 0x04);
+	*tmp3 = _mm_xor_si128(*tmp3, tmp4);
+	tmp4 = _mm_slli_si128(tmp4, 0x04);
+	*tmp3 = _mm_xor_si128(*tmp3, tmp4);
+	*tmp3 = _mm_xor_si128(*tmp3, tmp2);
+}
+
+// Special thanks to Intel for helping me
+// with ExpandAESKey256() and its subroutines
+static inline void ExpandAESKey256(char *keybuf)
+{
+	__m128i tmp1, tmp2, tmp3, *keys;
+	
+	keys = (__m128i *)keybuf;
+	
+	tmp1 = _mm_load_si128((__m128i *)keybuf);
+	tmp3 = _mm_load_si128((__m128i *)(keybuf+0x10));
+	
+	tmp2 = _mm_aeskeygenassist_si128(tmp3, 0x01);
+	ExpandAESKey256_sub1(&tmp1, &tmp2);
+	keys[2] = tmp1;
+	ExpandAESKey256_sub2(&tmp1, &tmp3);
+	keys[3] = tmp3;
+	
+	tmp2 = _mm_aeskeygenassist_si128(tmp3, 0x02);
+	ExpandAESKey256_sub1(&tmp1, &tmp2);
+	keys[4] = tmp1;
+	ExpandAESKey256_sub2(&tmp1, &tmp3);
+	keys[5] = tmp3;
+	
+	tmp2 = _mm_aeskeygenassist_si128(tmp3, 0x04);
+	ExpandAESKey256_sub1(&tmp1, &tmp2);
+	keys[6] = tmp1;
+	ExpandAESKey256_sub2(&tmp1, &tmp3);
+	keys[7] = tmp3;
+	
+	tmp2 = _mm_aeskeygenassist_si128(tmp3, 0x08);
+	ExpandAESKey256_sub1(&tmp1, &tmp2);
+	keys[8] = tmp1;
+	ExpandAESKey256_sub2(&tmp1, &tmp3);
+	keys[9] = tmp3;
+	
+	tmp2 = _mm_aeskeygenassist_si128(tmp3, 0x10);
+	ExpandAESKey256_sub1(&tmp1, &tmp2);
+	keys[10] = tmp1;
+	ExpandAESKey256_sub2(&tmp1, &tmp3);
+	keys[11] = tmp3;
+	
+	tmp2 = _mm_aeskeygenassist_si128(tmp3, 0x20);
+	ExpandAESKey256_sub1(&tmp1, &tmp2);
+	keys[12] = tmp1;
+	ExpandAESKey256_sub2(&tmp1, &tmp3);
+	keys[13] = tmp3;
+	
+	tmp2 = _mm_aeskeygenassist_si128(tmp3, 0x40);
+	ExpandAESKey256_sub1(&tmp1, &tmp2);
+	keys[14] = tmp1;
+}
+
+static const uint64_t keccakf_rndc[24] = 
+{
+    0x0000000000000001ULL, 0x0000000000008082ULL, 0x800000000000808aULL,
+    0x8000000080008000ULL, 0x000000000000808bULL, 0x0000000080000001ULL,
+    0x8000000080008081ULL, 0x8000000000008009ULL, 0x000000000000008aULL,
+    0x0000000000000088ULL, 0x0000000080008009ULL, 0x000000008000000aULL,
+    0x000000008000808bULL, 0x800000000000008bULL, 0x8000000000008089ULL,
+    0x8000000000008003ULL, 0x8000000000008002ULL, 0x8000000000000080ULL, 
+    0x000000000000800aULL, 0x800000008000000aULL, 0x8000000080008081ULL,
+    0x8000000000008080ULL, 0x0000000080000001ULL, 0x8000000080008008ULL
+};
+
+static const uint32_t keccakf_rotc[24] = 
+{
+    1,  3,  6,  10, 15, 21, 28, 36, 45, 55, 2,  14, 
+    27, 41, 56, 8,  25, 43, 62, 18, 39, 61, 20, 44
+};
+
+static const uint32_t keccakf_piln[24] = 
+{
+    10, 7,  11, 17, 18, 3, 5,  16, 8,  21, 24, 4, 
+    15, 23, 19, 13, 12, 2, 20, 14, 22, 9,  6,  1 
+};
+
+#define ROTL64(x, y)		(((x) << (y)) | ((x) >> (64 - (y))))
+#define bitselect(a, b, c) 	((a) ^ ((c) & ((b) ^ (a))))
+
+static void CNKeccakF1600(uint64_t *st)
+{
+	int i, round;
+	uint64_t t, bc[5];
+
+	for(round = 0; round < 24; ++round)
+	{
+		bc[0] = st[0] ^ st[5] ^ st[10] ^ st[15] ^ st[20] ^ ROTL64(st[2] ^ st[7] ^ st[12] ^ st[17] ^ st[22], 1UL);
+		bc[1] = st[1] ^ st[6] ^ st[11] ^ st[16] ^ st[21] ^ ROTL64(st[3] ^ st[8] ^ st[13] ^ st[18] ^ st[23], 1UL);
+		bc[2] = st[2] ^ st[7] ^ st[12] ^ st[17] ^ st[22] ^ ROTL64(st[4] ^ st[9] ^ st[14] ^ st[19] ^ st[24], 1UL);
+		bc[3] = st[3] ^ st[8] ^ st[13] ^ st[18] ^ st[23] ^ ROTL64(st[0] ^ st[5] ^ st[10] ^ st[15] ^ st[20], 1UL);
+		bc[4] = st[4] ^ st[9] ^ st[14] ^ st[19] ^ st[24] ^ ROTL64(st[1] ^ st[6] ^ st[11] ^ st[16] ^ st[21], 1UL);
+
+		st[0] ^= bc[4];
+		st[5] ^= bc[4];
+		st[10] ^= bc[4];
+		st[15] ^= bc[4];
+		st[20] ^= bc[4];
+
+		st[1] ^= bc[0];
+		st[6] ^= bc[0];
+		st[11] ^= bc[0];
+		st[16] ^= bc[0];
+		st[21] ^= bc[0];
+
+		st[2] ^= bc[1];
+		st[7] ^= bc[1];
+		st[12] ^= bc[1];
+		st[17] ^= bc[1];
+		st[22] ^= bc[1];
+
+		st[3] ^= bc[2];
+		st[8] ^= bc[2];
+		st[13] ^= bc[2];
+		st[18] ^= bc[2];
+		st[23] ^= bc[2];
+
+		st[4] ^= bc[3];
+		st[9] ^= bc[3];
+		st[14] ^= bc[3];
+		st[19] ^= bc[3];
+		st[24] ^= bc[3];
+
+		// Rho Pi
+		t = st[1];
+		for (i = 0; i < 24; ++i) {
+			bc[0] = st[keccakf_piln[i]];
+			st[keccakf_piln[i]] = ROTL64(t, keccakf_rotc[i]);
+			t = bc[0];
+		}
+
+		for(int i = 0; i < 25; i += 5)
+		{
+			uint64_t tmp1 = st[i], tmp2 = st[i + 1];
+
+			st[i] = bitselect(st[i] ^ st[i + 2], st[i], st[i + 1]);
+			st[i + 1] = bitselect(st[i + 1] ^ st[i + 3], st[i + 1], st[i + 2]);
+			st[i + 2] = bitselect(st[i + 2] ^ st[i + 4], st[i + 2], st[i + 3]);
+			st[i + 3] = bitselect(st[i + 3] ^ tmp1, st[i + 3], st[i + 4]);
+			st[i + 4] = bitselect(st[i + 4] ^ tmp2, st[i + 4], tmp1);
+		}
+
+		//  Iota
+		st[0] ^= keccakf_rndc[round];
+	}
+}
+
+void CNKeccak(uint64_t *output, uint64_t *input)
+{
+	uint64_t st[25];
+	
+	// Copy 72 bytes
+	for(int i = 0; i < 9; ++i) st[i] = input[i];
+	
+	st[9] = (input[9] & 0x00000000FFFFFFFFUL) | 0x0000000100000000UL;
+	
+	for(int i = 10; i < 25; ++i) st[i] = 0x00UL;
+	
+	// Last bit of padding
+	st[16] = 0x8000000000000000UL;
+	
+	CNKeccakF1600(st);
+	
+	memcpy(output, st, 200);
+}
+
+static inline uint64_t mul128(uint64_t a, uint64_t b, uint64_t *product_hi)
+{
+	uint64_t lo, hi;
+	
+	__asm__("mul %%rdx":
+	"=a" (lo), "=d" (hi):
+	"a" (a), "d" (b));
+	
+	*product_hi = hi;
+	
+	return lo;
+}
+
+extern int aesb_single_round(const uint8_t *in, uint8_t*out, const uint8_t *expandedKey);
+extern int aesb_pseudo_round(const uint8_t *in, uint8_t *out, const uint8_t *expandedKey);
+
+static inline size_t e2i(const uint8_t* a) {
+    return (*((uint64_t*) a) / AES_BLOCK_SIZE) & (MEMORY / AES_BLOCK_SIZE - 1);
+}
+
+static void mul(const uint8_t* a, const uint8_t* b, uint8_t* res) {
+    ((uint64_t*) res)[1] = mul128(((uint64_t*) a)[0], ((uint64_t*) b)[0], (uint64_t*) res);
+}
+
+static void mul_sum_xor_dst(const uint8_t* a, uint8_t* c, uint8_t* dst) {
+    uint64_t hi, lo = mul128(((uint64_t*) a)[0], ((uint64_t*) dst)[0], &hi) + ((uint64_t*) c)[1];
+    hi += ((uint64_t*) c)[0];
+
+    ((uint64_t*) c)[0] = ((uint64_t*) dst)[0] ^ hi;
+    ((uint64_t*) c)[1] = ((uint64_t*) dst)[1] ^ lo;
+    ((uint64_t*) dst)[0] = hi;
+    ((uint64_t*) dst)[1] = lo;
+}
+
+static void sum_half_blocks(uint8_t* a, const uint8_t* b) {
+    uint64_t a0, a1, b0, b1;
+
+    a0 = SWAP64LE(((uint64_t*) a)[0]);
+    a1 = SWAP64LE(((uint64_t*) a)[1]);
+    b0 = SWAP64LE(((uint64_t*) b)[0]);
+    b1 = SWAP64LE(((uint64_t*) b)[1]);
+    a0 += b0;
+    a1 += b1;
+    ((uint64_t*) a)[0] = SWAP64LE(a0);
+    ((uint64_t*) a)[1] = SWAP64LE(a1);
+}
+
+static inline void copy_block(uint8_t* dst, const uint8_t* src) {
+    ((uint64_t*) dst)[0] = ((uint64_t*) src)[0];
+    ((uint64_t*) dst)[1] = ((uint64_t*) src)[1];
+}
+
+static void swap_blocks(uint8_t* a, uint8_t* b) {
+    size_t i;
+    uint8_t t;
+    for (i = 0; i < AES_BLOCK_SIZE; i++) {
+        t = a[i];
+        a[i] = b[i];
+        b[i] = t;
+    }
+}
+
+static inline void xor_blocks(uint8_t* a, const uint8_t* b) {
+    ((uint64_t*) a)[0] ^= ((uint64_t*) b)[0];
+    ((uint64_t*) a)[1] ^= ((uint64_t*) b)[1];
+}
+
+static inline void xor_blocks_dst(const uint8_t* a, const uint8_t* b, uint8_t* dst) {
+    ((uint64_t*) dst)[0] = ((uint64_t*) a)[0] ^ ((uint64_t*) b)[0];
+    ((uint64_t*) dst)[1] = ((uint64_t*) a)[1] ^ ((uint64_t*) b)[1];
+}
+
+struct cryptonight_ctx {
+    uint8_t long_state[MEMORY] __attribute((aligned(16)));
+    union cn_slow_hash_state state;
+    uint8_t text[INIT_SIZE_BYTE] __attribute((aligned(16)));
+    uint64_t a[AES_BLOCK_SIZE >> 3] __attribute__((aligned(16)));
+    uint64_t b[AES_BLOCK_SIZE >> 3] __attribute__((aligned(16)));
+    uint8_t c[AES_BLOCK_SIZE] __attribute__((aligned(16)));
+    oaes_ctx* aes_ctx;
+};
+
+void cryptonight_hash(const char* input, char* output, uint32_t len) {
+    struct cryptonight_ctx *ctx = alloca(sizeof(struct cryptonight_ctx));
+    uint8_t ExpandedKey[256];
+    
+    CNKeccak(&ctx->state.hs, input);
+    
+   memcpy(ctx->text, ctx->state.init, INIT_SIZE_BYTE);
+    memcpy(ExpandedKey, ctx->state.hs.b, AES_KEY_SIZE);
+    ExpandAESKey256(ExpandedKey);
+    
+    __m128i *longoutput, *expkey, *xmminput;
+	longoutput = (__m128i *)ctx->long_state;
+	expkey = (__m128i *)ExpandedKey;
+	xmminput = (__m128i *)ctx->text;
+    
+    //for (i = 0; likely(i < MEMORY); i += INIT_SIZE_BYTE)
+    //    aesni_parallel_noxor(&ctx->long_state[i], ctx->text, ExpandedKey);
+    
+    for (int i = 0; __builtin_expect(i < 0x4000, 1); ++i)
+    {
+		for(int j = 0; j < 10; j++)
+		{
+			xmminput[0] = _mm_aesenc_si128(xmminput[0], expkey[j]);
+			xmminput[1] = _mm_aesenc_si128(xmminput[1], expkey[j]);
+			xmminput[2] = _mm_aesenc_si128(xmminput[2], expkey[j]);
+			xmminput[3] = _mm_aesenc_si128(xmminput[3], expkey[j]);
+			xmminput[4] = _mm_aesenc_si128(xmminput[4], expkey[j]);
+			xmminput[5] = _mm_aesenc_si128(xmminput[5], expkey[j]);
+			xmminput[6] = _mm_aesenc_si128(xmminput[6], expkey[j]);
+			xmminput[7] = _mm_aesenc_si128(xmminput[7], expkey[j]);
+		}
+		_mm_store_si128(&(longoutput[(i << 3)]), xmminput[0]);
+		_mm_store_si128(&(longoutput[(i << 3) + 1]), xmminput[1]);
+		_mm_store_si128(&(longoutput[(i << 3) + 2]), xmminput[2]);
+		_mm_store_si128(&(longoutput[(i << 3) + 3]), xmminput[3]);
+		_mm_store_si128(&(longoutput[(i << 3) + 4]), xmminput[4]);
+		_mm_store_si128(&(longoutput[(i << 3) + 5]), xmminput[5]);
+		_mm_store_si128(&(longoutput[(i << 3) + 6]), xmminput[6]);
+		_mm_store_si128(&(longoutput[(i << 3) + 7]), xmminput[7]);
+    }
+	
+	for (int i = 0; i < 2; i++) 
+    {
+	    ctx->a[i] = ((uint64_t *)ctx->state.k)[i] ^  ((uint64_t *)ctx->state.k)[i+4];
+	    ctx->b[i] = ((uint64_t *)ctx->state.k)[i+2] ^  ((uint64_t *)ctx->state.k)[i+6];
+    }
+
+	__m128i b_x = _mm_load_si128((__m128i *)ctx->b);
+    uint64_t a[2] __attribute((aligned(16))), b[2] __attribute((aligned(16)));
+    a[0] = ctx->a[0];
+    a[1] = ctx->a[1];
+	
+	for(int i = 0; __builtin_expect(i < 0x80000, 1); i++)
+	{	  
+	__m128i c_x = _mm_load_si128((__m128i *)&ctx->long_state[a[0] & 0x1FFFF0]);
+	__m128i a_x = _mm_load_si128((__m128i *)a);
+	uint64_t c[2];
+	c_x = _mm_aesenc_si128(c_x, a_x);
+
+	_mm_store_si128((__m128i *)c, c_x);
+	__builtin_prefetch(&ctx->long_state[c[0] & 0x1FFFF0], 0, 1);
+	
+	b_x = _mm_xor_si128(b_x, c_x);
+	_mm_store_si128((__m128i *)&ctx->long_state[a[0] & 0x1FFFF0], b_x);
+
+	uint64_t *nextblock = (uint64_t *)&ctx->long_state[c[0] & 0x1FFFF0];
+	uint64_t b[2];
+	b[0] = nextblock[0];
+	b[1] = nextblock[1];
+
+	{
+	  uint64_t hi, lo;
+	 // hi,lo = 64bit x 64bit multiply of c[0] and b[0]
+
+	  __asm__("mulq %3\n\t"
+		  : "=d" (hi),
+		"=a" (lo)
+		  : "%a" (c[0]),
+		"rm" (b[0])
+		  : "cc" );
+	  
+	  a[0] += hi;
+	  a[1] += lo;
+	}
+	uint64_t *dst = &ctx->long_state[c[0] & 0x1FFFF0];
+	dst[0] = a[0];
+	dst[1] = a[1];
+
+	a[0] ^= b[0];
+	a[1] ^= b[1];
+	b_x = c_x;
+	__builtin_prefetch(&ctx->long_state[a[0] & 0x1FFFF0], 0, 3);
+	}
+
+    memcpy(ctx->text, ctx->state.init, INIT_SIZE_BYTE);
+    memcpy(ExpandedKey, &ctx->state.hs.b[32], AES_KEY_SIZE);
+    ExpandAESKey256(ExpandedKey);
+    
+    //for (i = 0; likely(i < MEMORY); i += INIT_SIZE_BYTE)
+    //    aesni_parallel_xor(&ctx->text, ExpandedKey, &ctx->long_state[i]);
+    
+    for (int i = 0; __builtin_expect(i < 0x4000, 1); ++i) 
+	{	
+		xmminput[0] = _mm_xor_si128(longoutput[(i << 3)], xmminput[0]);
+		xmminput[1] = _mm_xor_si128(longoutput[(i << 3) + 1], xmminput[1]);
+		xmminput[2] = _mm_xor_si128(longoutput[(i << 3) + 2], xmminput[2]);
+		xmminput[3] = _mm_xor_si128(longoutput[(i << 3) + 3], xmminput[3]);
+		xmminput[4] = _mm_xor_si128(longoutput[(i << 3) + 4], xmminput[4]);
+		xmminput[5] = _mm_xor_si128(longoutput[(i << 3) + 5], xmminput[5]);
+		xmminput[6] = _mm_xor_si128(longoutput[(i << 3) + 6], xmminput[6]);
+		xmminput[7] = _mm_xor_si128(longoutput[(i << 3) + 7], xmminput[7]);
+		
+		for(int j = 0; j < 10; j++)
+		{
+			xmminput[0] = _mm_aesenc_si128(xmminput[0], expkey[j]);
+			xmminput[1] = _mm_aesenc_si128(xmminput[1], expkey[j]);
+			xmminput[2] = _mm_aesenc_si128(xmminput[2], expkey[j]);
+			xmminput[3] = _mm_aesenc_si128(xmminput[3], expkey[j]);
+			xmminput[4] = _mm_aesenc_si128(xmminput[4], expkey[j]);
+			xmminput[5] = _mm_aesenc_si128(xmminput[5], expkey[j]);
+			xmminput[6] = _mm_aesenc_si128(xmminput[6], expkey[j]);
+			xmminput[7] = _mm_aesenc_si128(xmminput[7], expkey[j]);
+		}
+		
+	}
+        
+    memcpy(ctx->state.init, ctx->text, INIT_SIZE_BYTE);
+	CNKeccakF1600(&ctx->state.hs);
+    extra_hashes[ctx->state.hs.b[0] & 3](&ctx->state, 200, output);
+}
+
+void cryptonight_fast_hash(const char* input, char* output, uint32_t len) {
+    union hash_state state;
+    hash_process(&state, (const uint8_t*) input, len);
+    memcpy(output, &state, HASH_SIZE);
+}
