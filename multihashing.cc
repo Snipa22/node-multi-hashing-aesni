432d8f8e7783a37083cb5e839d6814d9822cc18a
Alexander Blair <alex@snipanet.com>
2017-06-30 16:33:37 -0700

    Setting up to use Async via Nan.

--- multihashing.cc
+++ multihashing.cc
@@ -3,6 +3,7 @@
 #include <v8.h>
 #include <stdint.h>
 #include <nan.h>
+#include "multihashing_async.h"
 
 extern "C" {
     #include "cryptonight.h"
@@ -16,6 +17,7 @@ void callback(char* data, void* hint) {
 
 using namespace node;
 using namespace v8;
+using namespace Nan;
 
 NAN_METHOD(cryptonight) {
 
@@ -49,6 +51,47 @@ NAN_METHOD(cryptonight) {
     info.GetReturnValue().Set(
         returnValue
     );
+}
+
+class CNAsyncWorker : public Nan::AsyncWorker{
+    public:
+        CNAsyncWorker(Nan::Callback *callback, char * input, int input_len)
+            : Nan::AsyncWorker(callback), input(input), input_len(input_len){}
+        ~CNAsyncWorker() {}
+
+    void Execute () {
+        cryptonight_hash(input, output, input_len);
+      }
+
+    void HandleOKCallback () {
+        HandleScope scope;
+
+        Local<Value> argv[] = {
+            Null()
+          , v8::Local<v8::Value>(Nan::CopyBuffer(output, 32).ToLocalChecked())
+        };
+
+        callback->Call(2, argv);
+      }
+
+    private:
+        int input_len;
+        char * input[input_len];
+        char output[32];
+}
+
+NAN_METHOD(CNAsync) {
+
+    if (info.Length() != 2)
+        return THROW_ERROR_EXCEPTION("You must provide two arguments.");
+
+    Local<Object> target = info[0]->ToObject();
+    Callback *callback = new Nan::Callback(info[1].As<v8::Function>());
+
+    char * input = Buffer::Data(target);
+    uint32_t input_len = Buffer::Length(target);
+
+    Nan::AsyncQueueWorker(new CNAsyncWorker(callback, input, input_len));
 }
 
 NAN_MODULE_INIT(init) {
---
+++ multihashing_async.h
@@ -0,0 +1,8 @@
+#ifndef MULTIHASHING_ASYNC_H_
+#define MULTIHASHING_ASYNC_H_
+
+#include <nan.h>
+
+NAN_METHOD(CNAsync);
+
+#endif  // MULTIHASHING_ASYNC_H_
