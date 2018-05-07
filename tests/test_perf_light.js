7240f507065062911c9a86c17122dcac286d26b8
MoneroOcean <support@moneroocean.stream>
2018-05-07 11:43:32 +0200

    Fixed perf tests

--- tests/test_perf_light.js
+++ tests/test_perf_light.js
@@ -6,7 +6,7 @@ let input = Buffer.from("test");
 
 let start = Date.now();
 for (let i = ITER; i; -- i) {
-  multiHashing.cryptonight_light(input, 1);
+  multiHashing.cryptonight_light(input);
 }
 let end = Date.now();
 console.log("Perf: " + 1000 * ITER / (end - start) + " H/s");
