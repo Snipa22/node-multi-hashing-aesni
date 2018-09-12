aeee784ffd87ade066be9f8bd8d253892b405801
MoneroOcean <support@moneroocean.stream>
2018-09-12 12:19:48 +0200

    Fixed XAO/RTO indexes to mathc them to xmrig

--- tests/test_sync-xao.js
+++ tests/test_sync-xao.js
@@ -9,7 +9,7 @@ let lr = lineReader.createInterface({
 });
 lr.on('line', function (line) {
      let line_data = line.split(/ (.+)/);
-     let result = multiHashing.cryptonight(Buffer.from(line_data[1], 'hex'), 5).toString('hex');
+     let result = multiHashing.cryptonight(Buffer.from(line_data[1], 'hex'), 6).toString('hex');
      if (line_data[0] !== result){
          console.error(line_data[1] + ": " + result);
          testsFailed += 1;
