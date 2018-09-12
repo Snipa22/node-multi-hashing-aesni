ad8ad70f6bbb2c9bc28e0d4049ca8c05ba18f0b2
MoneroOcean <support@moneroocean.stream>
2018-09-12 12:21:11 +0200

    Switched perf to cn/2

--- tests/test_perf.js
+++ tests/test_perf.js
@@ -6,7 +6,7 @@ let input = Buffer.from("test");
 
 let start = Date.now();
 for (let i = ITER; i; -- i) {
-  multiHashing.cryptonight(input);
+  multiHashing.cryptonight(input, 8);
 }
 let end = Date.now();
 console.log("Perf: " + 1000 * ITER / (end - start) + " H/s");
