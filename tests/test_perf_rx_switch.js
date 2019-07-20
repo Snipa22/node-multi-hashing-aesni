dbdc503a67e11855ee6a03c51e851c93b4169890
MoneroOcean <support@moneroocean.stream>
2019-07-19 17:22:46 -0700

    Restore fixed seed usage

--- tests/test_perf_rx_switch.js
+++ tests/test_perf_rx_switch.js
@@ -4,16 +4,11 @@ let multiHashing = require('../build/Release/cryptonight-hashing');
 const ITER = 100;
 let input = Buffer.from("test");
 
-function pad(num, size) {
-    var s = '0000000000000000000000000000000000000000000000000000000000000000' + num;
-    return s.substr(s.length-size);
-}
-
 let start = Date.now();
 for (let i = ITER; i; -- i) {
-  multiHashing.randomx(input, Buffer.from(pad(i, 64), 'hex'), 17);
-  multiHashing.randomx(input, Buffer.from(pad(i, 64), 'hex'), 18);
-  multiHashing.randomx(input, Buffer.from(pad(i, 64), 'hex'), 0);
+  multiHashing.randomx(input, Buffer.from('0000000000000000000000000000000000000000000000000000000000000000', 'hex'), 17);
+  multiHashing.randomx(input, Buffer.from('0000000000000000000000000000000000000000000000000000000000000000', 'hex'), 18);
+  multiHashing.randomx(input, Buffer.from('0000000000000000000000000000000000000000000000000000000000000000', 'hex'), 0);
 }
 let end = Date.now();
 console.log("Perf: " + 1000 * ITER * 3 / (end - start) + " H/s");
